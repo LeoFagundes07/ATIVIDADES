02c683367a227be9460fc2921054ae8b3d35e009
Leonardo Fagundes <leeofagundes07@gmail.com>
2025-03-25 18:40:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+alert('Boas vindas ao nosso site');
+let nome1 = 'Lua';
+let idade1 = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro!Preencha todos os campos');
+let mensagemDeErro = 'Erro!preencha todos os campos';
+alert(mensagemDeErro);
+let nome = prompt('Digite o seu nome?');
+letidade  = prompt('Digite a sua idade?');
+letidade = prompt('Digite sua idade');
+if (idade >= 18) {
+alert ('Pode tirar habilitação!')
+};
